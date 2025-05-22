c98d59807cdc7dfea7779c9bded34da7a7958115
tania515 <vasiaeva.tatiana@gmail.com>
2025-05-22 12:39:49 +0300

    2

--- main.py
+++ main.py
@@ -1,29 +1,45 @@
 import os
 import shutil
+import time
+import chardet
 
+
+def time_file(path: str): 
+    modification_time = os.path.getmtime(path)
+    readable_time = time.ctime(modification_time)
+    return f'Время: {readable_time} \n'
+    
+    
 def do_if_exist (path: str):
     if os.path.exists(path):
-        return f'Путь {path} существует. \n'
+        return f'Попытка созднания, путь {path} существует. '
         
     else:
         os.makedirs(path)
-        return f'Папка создана {path}. \n'
+        return f'Папка создана {path}. '
  
  
- 
-path = 'logfile.log'
+path = './project_root/logs/logfile.log' 
+do_if_exist("./project_root/logs")
 if  not os.path.exists(path):
-    file = open('logfile.log','w', encoding = 'utf-8')
-    file.writelines('Файл logfile.txt создан \n')
+    file = open('./project_root/logs/logfile.log','w', encoding = 'utf-8')
+    file.writelines('Файл logfile.txt создан. ')
+    file.writelines(time_file(path))
     file.close()
 
-with open('logfile.log','a', encoding = 'utf-8') as file:
+with open('./project_root/logs/logfile.log','a', encoding = 'utf-8') as file:
     file.writelines(do_if_exist("./project_root"))
+    file.writelines(time_file(path))
     file.writelines(do_if_exist("./project_root/data/row"))
+    file.writelines(time_file(path))
     file.writelines(do_if_exist("./project_root/data/processed"))
+    file.writelines(time_file(path))
     file.writelines(do_if_exist("./project_root/logs"))
+    file.writelines(time_file(path))
     file.writelines(do_if_exist("./project_root/backups"))
+    file.writelines(time_file(path))
     file.writelines(do_if_exist("./project_root/output"))
+    file.writelines(time_file(path))
 
 #shutil.move('./logfile.log', "./project_root/logs")    
 
@@ -31,9 +47,23 @@ with open('./project_root/logs/logfile.log','a', encoding = 'utf-8') as file_1:
     with open('./project_root/data/row/data1.txt','w', encoding = 'utf-8') as file:
         file.writelines("Файл созднан \n")
         file.writelines("File is created \n")
-    file_1.writelines(f'файл data1.txt создан \n')
+    file_1.writelines(f'файл data1.txt создан. ')
+    file_1.writelines(time_file(path))
     with open('./project_root/data/row/data2.txt','w', encoding = 'CP1251') as file:
         file.writelines("Файл созднан \n")
         file.writelines("File is created \n")
-    file_1.writelines(f'файл data2.txt создан \n')
+    file_1.writelines(f'файл data2.txt создан. ')
+    file_1.writelines(time_file(path))
     
+files_and_dirs = os.listdir('./project_root/data/row')
+for file in files_and_dirs:    
+    #shutil.copy(f'./project_root/data/row/{file}',f'./project_root/data/processed{file}_processed')
+    with open(f'./project_root/data/row/{file}', 'rb') as fl:
+        lines = fl.readlines()
+        result = chardet.detect(lines[0])
+        encoding = result['encoding']
+        with open(f'./project_root/data/processed/{file}_processed','w', encoding = encoding) as file_2:
+            for line in lines:                
+                file_2.writelines(line.decode(encoding).rstrip('\n'))
+
+        
